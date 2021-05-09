9260a7fda87052ce35f8aa3258ef034580a187ba
Alfred Bratterud <alfred.bratterud@gmail.com>
2021-05-09 14:11:22 +0200

    SQL: Add image links, email and access levels

--- sql/my_database.sql
+++ sql/my_database.sql
@@ -2,22 +2,26 @@ CREATE DATABASE all_the_things;
 USE all_the_things;
 
 CREATE TABLE lonely_heroes (
-    id             INT unsigned NOT NULL AUTO_INCREMENT, 
+    id             INT unsigned NOT NULL AUTO_INCREMENT,     
     name           VARCHAR(150) NOT NULL,
+    email          VARCHAR(150),
     has_ponycopter BOOLEAN NOT NULL default FALSE,
+    access_level   INT unsigned NOT NULL default 0, #NOTE: This means anyone who can read can see the access levels.
+    image          VARCHAR(1024) default "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/The_Jolly_Nero.jpg/1024px-The_Jolly_Nero.jpg",
     PRIMARY KEY     (id)
 );
  
 # Not needed but nice for debug output
 DESCRIBE lonely_heroes;
 
-INSERT INTO lonely_heroes (name, has_ponycopter) VALUES
-('Superman', TRUE),
-('Batman', FALSE),
-('Chuck', TRUE),
-('Pinky Pie', TRUE);
-
-
+# Add your own gmail user here!
+INSERT INTO lonely_heroes (name, email, has_ponycopter, access_level, image) VALUES
+('Superman', NULL, TRUE, 0, "https://i.pinimg.com/originals/14/86/36/1486361592bfd0866cd900572833db0f.jpg"),
+('Batman', NULL, FALSE, 0, "https://i.ytimg.com/vi/RDhfnPSTqmk/maxresdefault.jpg"),
+('Chuck', NULL, TRUE, 100, DEFAULT),
+('Derek B.','derek.bipartisan@gmail.com',TRUE, 10, DEFAULT),
+('Alfred','alfred.bratterud@gmail.com',FALSE, 100, DEFAULT),
+('Pinky Pie',NULL,TRUE, 1000, "https://static.wikia.nocookie.net/mlp/images/b/b2/Pinkie_Pie_ID_S4E11.png");
 
 # Not needed but nice for debug output
 SELECT * FROM lonely_heroes;
