6e07e5a8cb035a618b88d023ed7b0c3773faf915
Alfred Bratterud <alfred.bratterud@gmail.com>
2021-05-09 14:35:02 +0200

    frontend: Integrate with Google sign-in and new backend

--- frontend/fullstack.js
+++ frontend/fullstack.js
@@ -1,56 +1,159 @@
 
 console.log("Fullstack frontend loaded");
 
-var images = [
-    "https://static.wikia.nocookie.net/mlp/images/b/b2/Pinkie_Pie_ID_S4E11.png",
-    "https://media.karousell.com/media/photos/products/2017/02/02/lego_batman_lobster_figure_1486041715_4eed985e.jpg",
-    "https://static.dw.com/image/18463014_303.jpg",
-    "https://static.wikia.nocookie.net/mlp/images/b/b2/Pinkie_Pie_ID_S4E11.png"
-]
-
 var users = []
+var logged_in = null
+var h_row1 = null
+
+var copter_img = "https://www.puppentoys.com/WebRoot/ce_es/Shops/940395445/5320/45C4/8DBF/84BC/3270/C0A8/8010/C606/A5935-2.jpg"
+var nocopter_img = "http://www.skyblue-pink.com/wp-content/uploads/2013/08/sadbatman.jpg"
 
 function load_copter(){
-    var copter_img = "https://www.puppentoys.com/WebRoot/ce_es/Shops/940395445/5320/45C4/8DBF/84BC/3270/C0A8/8010/C606/A5935-2.jpg"
-    var nocopter_img = "http://www.skyblue-pink.com/wp-content/uploads/2013/08/sadbatman.jpg"
     row = this.parentElement.parentElement;
-    copter = Boolean(users[row.id - 1][2]);
-    console.log(`Loading copter for ${row.id}. Has ponycopter? ${users[row.id - 1][2]}`);
+    user_id = row.id;
+    user = users[user_id];
+    console.log(`Loading copter for user ${user_id}`)
+    copter = Boolean(user[3]);
     var h_img = row.querySelector(".card-image");
     h_img.src = copter ? copter_img : nocopter_img;
 }
 
+function remove_users(){
+    var h_list = document.getElementById("user_list");
+    var rows = h_list.querySelectorAll(".row");
+    console.log(`Removign users: ${rows}`);
+    rows.forEach(node => {
+        console.log(`removing row ${node.id}`);
+        node.remove();
+    });
+}
+
 function render_users(data){
     console.log(`Rendering ${data}`)
+
     var h_list = document.getElementById("user_list");
-    var h_row1 = document.getElementById("first_row");
+    if (h_row1 == null) {
+        h_row1 = document.getElementById("first_row");
+    }
     console.log(`My DOM list: ${h_list}`)
-    for (i in data){
-        users[i] = data[i];
-        [id, n, copter] = data[i];
+    console.log(`data length: ${data.length} data: ${data}`)
+
+    for (var i = 0; i < data.length; i++){
+        console.log(`Rendering element ${data[i]}\n\n`);
+
+        // NOTE: This is very brittle - objects with named keys are better
+        [id, n, email, copter, access, img] = data[i];
+
+        /*  Add information from the google profile if this is
+            the logged in user */
+        if (logged_in && email == logged_in.getEmail()){
+            n = logged_in.getName();
+            img = logged_in.getImageUrl();
+            // Google has a nice API for resizing images
+            // from https://developers.google.com/people/image-sizing
+            img = img.split("=")[0]+"=s300";
+        }
+
+        console.log(`Image: ${img}`)
 
         var cloned_row = h_row1.cloneNode(true);
-        cloned_row.id = id;
+        cloned_row.id = i;
 
         var h_title = cloned_row.querySelector(".card-title");
         h_title.innerHTML =  id.toString() + ". Name: " + n;
-        
+
         var h_img = cloned_row.querySelector(".card-image");
-        h_img.src = images[id - 1];
+        h_img.src = img
 
         var h_btn = cloned_row.querySelector(".btn");
         h_btn.onclick = load_copter;
-        console.log(`Added onclick: ${h_btn.onclick}`)
-
+        console.log(`Added onclick`)
         console.log(`ID: ${id}, Name: ${n}, Copter? ${Boolean(copter)}`);
-        h_list.appendChild(cloned_row);
+
+        if(logged_in && logged_in.getEmail() == email){
+            h_list.insertBefore(cloned_row, h_list.querySelector(".row"))
+        } else {
+            h_list.appendChild(cloned_row);
+        }
 
     }
     h_row1.remove();
 }
 
-fetch("./api/users")
-    .then(response => response.json())
-    .then(data => render_users(data));
+function load_users(){
+    users = []
+    fetch("./api/users")
+        .then(response => response.json())
+        .then(data => {
+            users = data;
 
-console.log("Fetch in progress");
+            if (h_row1 != null){
+                remove_users();
+            }
+            render_users(data)
+        });
+
+    console.log("Fetch in progress");
+}
+
+load_users();
+
+// Google sign-on
+console.log("Signin javascript loading");
+
+var id_token = null;
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    id_token = googleUser.getAuthResponse().id_token;
+    console.log(`ID Token to pass to server: ${id_token}`)
+
+    logged_in = profile;
+
+    // Render again to get the logged in users card
+    // updated from the google profile
+    remove_users();
+    render_users(users);
+}
+
+console.log("Adding signOut function");
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      logged_in = null;
+      remove_users();
+      load_users();
+    });
+    id_token = null;
+}
+
+
+function danger_return(data){
+    console.log("Authenticated request returned");
+    console.log(data);
+    remove_users();
+    render_users(data);
+}
+
+function danger(){
+    console.log("Danger!");
+    if (id_token == null) {
+        alert("ILLEGAL ACCESS ATTEMPT REGISTERED");
+        return;
+    }
+
+    fetch("./api/users", {
+        headers: {
+            'Authorization' : id_token
+        }
+    }).then(response => response.json())
+    .then(data => danger_return(data));
+
+}
+
